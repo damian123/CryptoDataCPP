e80be6d7f1096ebf0201ee3983be2c4a03870bd7
schouwlars <lars@schouw.com>
2021-05-19 15:47:39 +0900

    writeup docs for the rest client methods

--- cpp/src/feed/exchange/FTX/rest/client.h
+++ cpp/src/feed/exchange/FTX/rest/client.h
@@ -11,58 +11,58 @@ namespace ftx {
 class RESTClient
 {
   public:
+    //! Default constructor
     RESTClient();
 
-    /// <summary>
-    /// List all futures on FTX
-    /// </summary>
-    /// <returns></returns>
+    /*! 
+     * List all futures on FTX.
+     * @return Market data and instrument spec for all listed futures.
+     */    
     json list_futures();
-
-    /// <summary>
-    /// List all markets on FTX
-    /// </summary>
-    /// <returns></returns>
+    
+    /*! List all markets on FTX, where a market is a listed security. 
+     * @return A set of markets showing market data and instrument spec.
+     */ 
     json list_markets();
-
-    /// <summary>
-    /// Get the order book for a market
-    /// </summary>
-    /// <param name="market"></param>
-    /// <param name="depth"></param>
-    /// <returns></returns>
+    
+    /*!
+     * Get the order book for a market.
+     * @param market
+     * @param depth
+     * @return The list of orders in the order book.
+     */
     json get_orderbook(const std::string market, int depth = 100);
 
-    /// <summary>
-    /// Get trades for a market (security code)
-    /// </summary>
-    /// <param name="market"></param>
-    /// <returns></returns>
+    /*!
+     * Get trades for a market (security code)
+     * @param market
+     * @return The users executed trades for the market.
+     */
     json get_trades(const std::string market);
 
-    /// <summary>
-    /// Get account information. 
-    /// </summary>
-    /// <returns></returns>
+    /*!
+     * Get account information.
+     * @return The account information for the user.
+     */
     json get_account_info();
 
-    /// <summary>
-    /// Get open orders. 
-    /// </summary>
-    /// <returns></returns>
+    /*!
+     * Get open orders. 
+     * @return
+     */
     json get_open_orders();
 
-    /// <summary>
-    /// Place an limit order on the exchange
-    /// </summary>
-    /// <param name="market">symbol code for the security</param>
-    /// <param name="side">buy or sell</param>
-    /// <param name="price">limit price</param>
-    /// <param name="size">notional amount of base currency to trade</param>
-    /// <param name="ioc">true if "immediate or cancel" order type, else false</param>
-    /// <param name="post_only">true if the order will not be allowed to take. Post-only orders will only pay maker fees. The default value is false.</param>
-    /// <param name="reduce_only">true if the order will only reduce your position size. These orders can only close your positions. The default value is false.</param>
-    /// <returns></returns>
+    /*!
+     * Place an limit order on the exchange.
+     * @param market The symbol code for the security.
+     * @param side buy or sell.
+     * @param price The price limit.
+     * @param size The notional amount of base currency.
+     * @param ioc true if "immediate or cancel" order type, else false.
+     * @param post_only true if the order will not be allowed to take. Post-only orders will only pay maker fees. The default value is false.
+     * @param reduce_only true if the order will only reduce your position size. These orders can only close your positions. The default value is false.
+     * @return
+     */    
     json place_order(const std::string market,
                      const std::string side,
                      double price,
@@ -71,16 +71,16 @@ class RESTClient
                      bool post_only = false,
                      bool reduce_only = false);
     
-    /// <summary>
-    /// Place a market order on the exchange
-    /// </summary>
-    /// <param name="market">symbol code for the security</param>
-    /// <param name="side">buy or sell</param>
-    /// <param name="size">notional amount of base currency to trade</param>
-    /// <param name="ioc">true if "immediate or cancel" order type, else false</param>
-    /// <param name="post_only">true if the order will not be allowed to take. Post-only orders will only pay maker fees. The default value is false.</param>
-    /// <param name="reduce_only">true if the order will only reduce your position size. These orders can only close your positions. The default value is false.</param>
-    /// <returns></returns>
+    /*!
+     * Place a market order on the exchange.
+     * @param market The symbol code for the security.
+     * @param side buy or sell
+     * @param size The notional amount of base currency
+     * @param ioc true if "immediate or cancel" order type, else false
+	 * @param post_only true if the order will not be allowed to take. Post-only orders will only pay maker fees. The default value is false.
+	 * @param reduce_only true if the order will only reduce your position size. These orders can only close your positions. The default value is false.
+     * @return
+     */        
     json place_order(const std::string market,
                      const std::string side,
                      double size,
@@ -88,30 +88,30 @@ class RESTClient
                      bool post_only = false,
                      bool reduce_only = false);
 
-    /// <summary>
-    /// Cancle an order
-    /// </summary>
-    /// <param name="order_id"></param>
-    /// <returns></returns>
+    /*!
+     * Cancel an order.
+     * @param order_id
+     * @return The order queued for cancellation.
+     */
     json cancel_order(const std::string order_id);
 
-    /// <summary>
-    /// Get filled orders
-    /// </summary>
-    /// <returns></returns>
+    /*!
+     * Get filled orders
+     * @return Fills for the executed orders.
+     */
     json get_fills();
 
-    /// <summary>
-    /// Get wallet balances
-    /// </summary>
-    /// <returns></returns>
+    /*!
+     * Get wallet balances
+     * @return The asset balances for the wallet
+     */
     json get_balances();
 
-    /// <summary>
-    /// Get wallet deposit address
-    /// </summary>
-    /// <param name="ticker">symbol code for security</param>
-    /// <returns></returns>
+    /*!
+     * Get wallet deposit address for a ticker code.
+     * @param ticker The symbol code for security.
+     * @return
+     */
     json get_deposit_address(const std::string ticker);
 
   private:
